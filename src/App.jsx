670bf7cb0290cbca9c6d56571418459420801e64
FayyazAbdulla <abdx2228@gmail.com>
2025-02-14 22:54:16 +0530

    started the UI development

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-// Desc: Main App component
-import ComingSoon from './pages/ComingSoon'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Index from "./pages/index";
+import FoodDetailScreen from "./pages/FoodDetail";
+import PlaceOrder from "./pages/PlaceOrder";
 
 function App() {
-  
-
   return (
     <>
-      <ComingSoon />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/foods/:title" element={<FoodDetailScreen />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
